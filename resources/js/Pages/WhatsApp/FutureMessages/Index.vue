558786eb6f4beb138e6cd197511894f58717af86
gereiz <georgie.reis@outlook.com>
2026-01-26 17:41:28 -0300

    Ajustes na Faturas atrasadas e futuras

--- resources/js/Pages/WhatsApp/FutureMessages/Index.vue
+++ resources/js/Pages/WhatsApp/FutureMessages/Index.vue
@@ -86,7 +86,7 @@ const statusClass = (status) => {
         </template>
 
         <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="mx-auto max-w-screen-2xl sm:px-6 lg:px-8">
                 
                 <!-- Filters -->
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg mb-6 p-6">
